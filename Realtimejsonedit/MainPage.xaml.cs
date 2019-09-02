cc6a82a6286ea94b975e1986fd5b54120d7932fe
fausto <operyde@gmail.com>
2019-09-02 10:11:08 -0400

    realtime change

--- Realtimejsonedit/MainPage.xaml.cs
+++ Realtimejsonedit/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
@@ -23,26 +24,60 @@ using Windows.UI.Xaml.Navigation;
 namespace Realtimejsonedit
 { 
     public sealed partial class MainPage : Page
-    {
-       // Notes notes = new Notes();
-     
-        //, INotifyPropertyChanged
-         public string editpath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Notas.json" );
+    { 
+        private dynamic jsonObj;
+        public string editpath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Notas.json" );
         public ObservableCollection<Notes> Mynotes; 
-       
         
-    
-
         public MainPage()
         {
             this.InitializeComponent();
+            string json = File.ReadAllText(editpath);
+            jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
          
-
-            // Load data of Notas.json to Listview
-            LoadUpdate();
-
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(NewValue));
         }
 
+
+        private void Titulo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int indice = listNotas.SelectedIndex;
+            jsonObj["notes"][indice]["title"] = titulo.Text;
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj);
+            editor.TextDocument.SetText(Windows.UI.Text.TextSetOptions.None, output);
+            File.WriteAllText(editpath, output);
+        }
+
+        
+
+        //private void ListNotas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    string json = File.ReadAllText(editpath);
+        //    dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+        //    titulo.Text = jsonObj["notes"][listNotas.SelectedIndex]["title"];
+
+        //}
+     
+
+        
+    }
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public string editpath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Notas.json");
+        public ObservableCollection<Notes> Mynotes { get; set; }
+        public ViewModel()
+        {
+            LoadUpdate();
+            SetSelectIndex(0);
+
+        }
+        private void SetSelectIndex(int index)
+        {
+            SelectItem = Mynotes[index];
+        }
         private void LoadUpdate()
         {
             using (StreamReader file = File.OpenText(editpath))
@@ -50,54 +85,30 @@ namespace Realtimejsonedit
                 var json = file.ReadToEnd();
                 baseNotes mainnotes = JsonConvert.DeserializeObject<baseNotes>(json);
                 Mynotes = new ObservableCollection<Notes>();
-
                 foreach (var item in mainnotes.notes)
                 {
                     Mynotes.Add(new Notes { title = item.title });
-                } 
-                listNotas.ItemsSource = null;
-                listNotas.ItemsSource = Mynotes;
-                listNotas.SelectedIndex = 0;
+
+                }
+
             }
         }
- 
-        private void ListNotas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        public void OnPropertyChanged([CallerMemberName] string propertyname = null)
         {
-             string json = File.ReadAllText(editpath);
-            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-            titulo.Text = jsonObj["notes"][listNotas.SelectedIndex]["title"];
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
 
         }
+        private Notes _selectItem;
 
-        private void Titulo_KeyDown(object sender, KeyRoutedEventArgs e)
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Notes SelectItem
         {
-            #region
-            string json = File.ReadAllText(editpath);
-            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-            int indice = listNotas.SelectedIndex;
-            jsonObj["notes"][indice]["title"] = titulo.Text;
-
-            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj);
-            File.WriteAllText(editpath, output);
-            // Show json file text in RicheditBox
-            editor.TextDocument.SetText(Windows.UI.Text.TextSetOptions.None, output);            
-
-            
-            //Problem
-            Binding myBinding = new Binding();
-            myBinding.Source = Mynotes[indice];
-            myBinding.Path = new PropertyPath("title");
-            myBinding.Mode = BindingMode.TwoWay;
-            myBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
-            BindingOperations.SetBinding(titulo, TextBox.TextProperty, myBinding);
-            #endregion
-            //ListViewItem text = listNotas.SelectedItem as ListViewItem;
-            //text.Content = "dsdg";
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(NewValue));
+            get { return _selectItem; }
+            set
+            {
+                _selectItem = value;
+                OnPropertyChanged();
+            }
         }
     }
 }
